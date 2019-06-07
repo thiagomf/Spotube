f2c9f8e9d11841dd40138210e4f3e7f478f5c37b
Thiago M Faria <thiagomachadofaria@correio.itau.com.br>
2019-06-07 10:34:52 -0300

    Initial Commit

---
+++ Spotube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spotube
+//
+//  Created by Thiago Machado Faria on 07/06/2019.
+//  Copyright © 2019 Thiago Machado Faria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
