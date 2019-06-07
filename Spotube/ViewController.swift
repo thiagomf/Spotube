719e3b1c4320498642969bceca55b42e1af41772
Thiago M Faria <thiagomachadofaria@correio.itau.com.br>
2019-06-07 10:56:55 -0300

    Commit hello word

--- Spotube/ViewController.swift
+++ Spotube/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Hello world")
+        print("Hello worldttttt")
     }
 
 
