97a09307db25e13935f6a19f47a1ca22f9ff0373
Thiago M Faria <thiagomachadofaria@correio.itau.com.br>
2019-06-07 17:02:00 -0300

    Cria framework Spotify

---
+++ Spotube/Spotube-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Spotube-Bridging-Header.h
+//  Spotube
+//
+//  Created by Thiago Machado Faria on 07/06/2019.
+//  Copyright © 2019 Thiago Machado Faria. All rights reserved.
+//
+
+#ifndef Spotube_Bridging_Header_h
+#define Spotube_Bridging_Header_h
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#endif /* Spotube_Bridging_Header_h */
